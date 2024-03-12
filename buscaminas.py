09796e0b32da85e37c77e5d8690d1ba96dade36d
Nicolasguerra9 <56521134+Nicolasguerra9@users.noreply.github.com>
2024-03-12 19:33:02 +0100

    marcadores de bombas

--- buscaminas.py
+++ buscaminas.py
@@ -5,9 +5,10 @@ import time
 class BuscaminasGUI:
     def __init__(self, master):
         self.master = master
-        self.filas = 5
-        self.columnas = 5
+        self.filas = 10
+        self.columnas = 10
         self.num_bombas = 5
+        self.bombas_marcadas = 0
         self.tiempo_inicio = None
         self.botones = []
 
@@ -33,7 +34,15 @@ class BuscaminasGUI:
             self.mostrar_mensaje("¡Has perdido! Una bomba ha explotado.")
         else:
             num_bombas_alrededor = self.contar_bombas_alrededor(fila, columna)
-            self.botones[fila][columna].config(text=str(num_bombas_alrededor), state='disabled', relief='sunken', bg='light green')
+            self.botones[fila][columna].config(text=str(num_bombas_alrededor), state='disabled', relief='sunken', bg='light blue')
+            if num_bombas_alrededor == 0:
+                self.expandir_celdas(fila, columna)
+
+    def expandir_celdas(self, fila, columna):
+        for i in range(max(0, fila - 1), min(self.filas, fila + 2)):
+            for j in range(max(0, columna - 1), min(self.columnas, columna + 2)):
+                if self.botones[i][j]['state'] == 'normal' and self.contar_bombas_alrededor(i, j) == 0:
+                    self.mostrar_bomba(i, j)
 
     def contar_bombas_alrededor(self, fila, columna):
         count = 0
@@ -52,14 +61,35 @@ class BuscaminasGUI:
         for i in range(self.filas):
             fila_botones = []
             for j in range(self.columnas):
-                boton = tk.Button(self.master, text=' ', width=4, height=2, bg='grey',
-                                  command=lambda f=i, c=j: self.mostrar_bomba(f, c))
+                boton = tk.Button(self.master, text=' ', width=4, height=2, bg='grey')
+                boton.bind('<Button-1>', lambda event, f=i, c=j: self.primer_clic(event, f, c))
+                boton.bind('<Button-3>', lambda event, f=i, c=j: self.marcar_bomba(event, f, c))
                 boton.grid(row=i, column=j)
                 fila_botones.append(boton)
             self.botones.append(fila_botones)
 
         self.mensaje = tk.Label(self.master, text="")
         self.mensaje.grid(row=self.filas, columnspan=self.columnas)
+
+    def primer_clic(self, event, fila, columna):
+        if self.tiempo_inicio is None:
+            self.tiempo_inicio = time.time()
+            for i in range(max(0, fila - 1), min(self.filas, fila + 2)):
+                for j in range(max(0, columna - 1), min(self.columnas, columna + 2)):
+                    if self.contar_bombas_alrededor(i, j) == 0:
+                        self.mostrar_bomba(i, j)
+                    else:
+                        self.mostrar_bomba(fila, columna)
+        else:
+            self.mostrar_bomba(fila, columna)
+
+    def marcar_bomba(self, event, fila, columna):
+        if self.botones[fila][columna]['text'] == ' ' and self.bombas_marcadas < self.num_bombas:
+            self.botones[fila][columna].config(text='M', bg='yellow', state='disabled')
+            self.bombas_marcadas += 1
+        elif self.botones[fila][columna]['text'] == 'M':
+            self.botones[fila][columna].config(text=' ', bg='grey', state='normal')
+            self.bombas_marcadas -= 1
 
 def main():
     root = tk.Tk()
